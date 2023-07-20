bf4934996cd0b30eb748fcaa07527d91e62d6d4d
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-07-20 14:33:34 +0100

    todolist

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+import classes from "./TodoList.module.css"
+const TodoList:React.FC<{text: string}> = (props) => {
+  return (
+    <li className={classes.item}>{props.text}</li>
+  )
+}
+
+export default TodoList
