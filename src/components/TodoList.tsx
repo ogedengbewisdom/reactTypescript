b8fa3859c7bef6b3f257a9525c69d7346ae8d32c
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-07-21 09:20:27 +0100

    LIst of todo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,8 @@
 
-import React from 'react'
-import classes from "./TodoList.module.css"
+import { ListItem } from '@chakra-ui/react';
 const TodoList:React.FC<{text: string; onRemove: () => void}> = (props) => {
   return (
-    <li className={classes.item} onClick={props.onRemove}>{props.text}</li>
+    <ListItem fontWeight="semibold" borderRadius="5px" boxShadow="2px 2px 5px rgb(195, 196, 184)" border="1px solid rgb(195, 196, 184)" margin="1rem" padding="0.6rem" onClick={props.onRemove}>{props.text}</ListItem>
   )
 }
 
