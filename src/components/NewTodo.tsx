94ab5f8a414c22de11f92e49329563f8cc8be8e7
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-07-21 09:19:56 +0100

    create todo

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,27 +1,34 @@
-import { FormEvent, useContext, useRef } from "react"
-import classes from "./NewTodo.module.css"
-import { TodoContext } from "../store/todosContext"
-const NewTodo: React.FC = (props) => {
-   
+import React, { FormEvent, useContext, useRef } from 'react'
+import {
+    Button,
+    FormControl,
+    FormLabel,
+    Input
+  } from '@chakra-ui/react';
+import { TodoContext } from '../store/todosContext';
+
+
+const NewTodo:React.FC = (props) => {
     const todoCtx = useContext(TodoContext)
     const inputRef = useRef<HTMLInputElement>(null)
     const submitHandler = (event: FormEvent) => {
         event.preventDefault();
-        const enteredText = inputRef.current!.value;
-        if(enteredText.trim().length === 0) {
-            return;
-        } else {
-            todoCtx.addTodo(enteredText)
-        }
+    const enteredTodos = inputRef.current!.value;
+    if (enteredTodos.trim().length === 0) {
+        return
+    } else {
+        todoCtx.addTodo(enteredTodos)
     }
-
-    return (
-        <form onSubmit={submitHandler} className={classes.form}>
-            <label htmlFor="text">Todo</label>
-            <input type="text" id="text" ref={inputRef} required />
-            <button>Add Todo</button>
-        </form>
-    )
+    }
+  return (
+    <form onSubmit={submitHandler}>
+        <FormControl mt="1rem" mb="0.8rem">
+            <FormLabel fontSize={"1.5rem"}>Enter Todo</FormLabel>
+            <Input type='text'padding="1.4rem" boxShadow={"2px 2px 10px rgb(151, 152, 146)"} border={"1px solid rgb(195, 196, 184)"}  borderRadius={"5px"} ref={inputRef} />
+        </FormControl>
+        <Button type='submit' boxShadow={"2px 2px 10px rgb(151, 152, 146)"}>Add Todo</Button>
+    </form>
+  )
 }
 
 export default NewTodo
